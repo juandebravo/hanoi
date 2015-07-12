13a4aea24e2a4bbf7d0637df9d828eb7a6e32c3e
juandebravo <juandebravo@gmail.com>
2015-07-12 19:36:41 +0200

    include version automatically in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hanoi',
-    description='Toggle on/off features. Rollout python port',
-    version='{VERSION}',
-    url='https://github.com/juandebravo/hanoi',
     author='Juan de Bravo',
     author_email='juandebravo@gmail.com',
-    packages=find_packages(),
+    description='Toggle on/off features. Rollout python port',
     include_package_data=True,
     install_requires=['redis'],
+    name='hanoi',
+    packages=find_packages(),
     tests_require=['nose', 'pyshould', 'redis-py'],
     test_suite='nose.collector',
+    url='https://github.com/juandebravo/hanoi',
+    version='0.0.3',
     zip_safe=False,
 )
